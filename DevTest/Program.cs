8feae5411ce92d88d3c933d2249e8ad970f8c9e8
Alejandro Gonzalez <intuido@yahoo.com>
2020-11-12 19:30:33 -0300

    Add project files.

---
+++ DevTest/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DevTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Console.WriteLine("4586 {0}", HumanFriendlyInteger.IntegerToWritten(4586));
+
+            var numeros = new List<int>
+            {
+                0, 1, 2, 5, 8, 12, 15, 18, 20, 21,22,23,29, 30, 40, 42, 46, 98, 100, 101,111, 200,222, 300,343,400, 404,411,421,431,441, 444,451,461, 471, 
+                481, 491,500,508, 600, 666, 700, 745, 800,816,900, 901,915, 999, 1000, 1001, 1010, 1100,
+                1203, 1220, 1224, 10000, 20000, 20123, 
+                21312, 45456, 53135,
+                1000000, 1111111, 1180000, 1180400, 1180430,
+                1200000, 1250000, 2000000, 12000000, 12345678, -3
+            };
+            numeros.ForEach(x => Console.WriteLine("{0} {1}", x, NumerosLeibles.Int2Espanol(x)));
+        }
+    }
+
+    public static class NumerosLeibles
+    {
+        private static readonly string[] Unidades = new string[] { "cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve" , "diez", "once", "doce",
+            "trece", "catorce", "quince" };
+        private static readonly string[] Decenas = new string[] { "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa" };
+        static string[] grandes = { "", " Mil", " Millón", " Billón" };
+
+       
+        private static string Espanol(int numero)
+        {
+            if (numero < 16)
+                return Unidades[numero];
+            if (numero < 20)
+                return $"dieci{Espanol(numero - 10)}";
+            if (numero == 20)
+                return "veinte";
+            if (numero < 30)
+                return $"veinti{Espanol(numero - 20)}";
+            //decenas
+            switch (numero)
+            {
+                case 30:
+                case 40:
+                case 50:
+                case 60:
+                case 70:
+                case 80:
+                case 90:
+                    return Decenas[numero / 10 - 3];
+            }
+
+            if (numero < 100) //entre 31 y 99 pero no termina en 0, 
+            {
+                Math.DivRem(numero, 10, out int resto10);
+                return $"{Espanol(numero - resto10)} y {Espanol(resto10)}";
+            }
+            //cientos
+            switch (numero)
+            {
+                case 100:
+                    return "cien";
+                case 200:
+                case 300:
+                case 400:
+                    return $"{Espanol(numero / 100)}cientos";
+                case 500:
+                    return "quinientos";
+                case 600:
+                    return $"{Espanol(numero / 100)}cientos";
+                case 700:
+                    return "setecientos";
+                case 800:
+                    return $"{Espanol(numero / 100)}cientos";
+                case 900:
+                    return "novecientos";
+                case 1000:
+                    return "mil";
+            }
+            //entre 100 y 200
+            if (numero < 200) 
+                return $"ciento {Espanol(numero - 100)}";
+            
+            //menor que mil, ignorado hasta ahora por ej doscientos + resto
+            if (numero < 1000)
+            {
+                Math.DivRem(numero, 100, out int resto100);
+                return $"{Espanol(numero - resto100)} {Espanol(resto100)}";
+            }
+                
+            
+            //entre mil y 2000, mil + resto
+            if (numero < 2000)
+                return $"mil {Espanol(numero % 1000)}";
+            
+            if (numero < 1000000)
+            {
+                //splitear en miles
+                var result = new StringBuilder(Espanol(numero / 1000));
+                result.Append(" mil");
+                if ((numero % 1000) > 0)
+                {
+                    result.Append(" ");
+                    result.Append(Espanol(numero % 1000));
+                }
+
+                return result.ToString();
+            }
+            
+            //llegamos al millon
+            if (numero == 1000000)
+                return "un millón";
+            //menos de 2: tengo que usar un millon , en vez de n millones
+            if (numero < 2000000)
+                return $"un millón {Espanol(numero % 1000000)}";
+
+            //partir en millones
+            var millones = new StringBuilder(Espanol(numero / 1000000));
+            millones.Append(" millones");
+            Math.DivRem(numero, 1000000, out int resto);
+            if (resto> 0)
+            {
+                millones.Append(" ");
+                millones.Append(Espanol(resto));
+            }
+
+            return millones.ToString();
+        }
+
+        public static string Int2Espanol(int n)
+        {
+            if (n < 0)
+            {
+                return "menos " + Int2Espanol(-n);
+            }
+
+            return Espanol(n);
+        }
+    }
+}
